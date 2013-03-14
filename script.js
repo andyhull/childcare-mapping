99c2bc2d27ca19589961eaf35584be1b9d51256e
Andy Hull <andyhull3@gmail.com>
2013-03-13 20:19:35 -0700

    lame commit

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ $('#map').mapbox('andyhull.map-qflr4pt1', function(map, tilejson) {
     $('p.description').text(tilejson.description);
 
 console.log(childcarecenters);
-console.log(mapbox.markers);
+console.log(map);
     var container = $('#markerfilters');
     $.each(childcarecenters.markers(), function(index, m) {
         console.log("hey"+m);
