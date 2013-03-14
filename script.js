3635e0813a246ccb5c470b07cc9c45d91e34eb0d
Andy Hull <andyhull3@gmail.com>
2013-03-14 10:24:10 -0700

    adding tooltips

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 $('#map').mapbox('andyhull.map-qflr4pt1', function(map, tilejson) {
     // style, the file must be on the same domain name as the map,
     // or loading will not work due to cross-domain request restrictions
-    // var childcarecenters = mapbox.markers.layer().url('data/childcarecenters.geojson');
-    // mapbox.markers.interaction(childcarecenters);
-    // map.addLayer(childcarecenters);
 
     var container = $('#markerfilters');
     var childcarecenters = mapbox.markers.layer();
@@ -26,13 +23,13 @@ $('#map').mapbox('andyhull.map-qflr4pt1', function(map, tilejson) {
             .text('Infant')
             .bind('click', filterInfant);
         container.append(el);
-        var el = $(document.createElement('a'))
-            .addClass('markerfilter')
-            .attr('href', '#preschool')
-            .css('background-image', 'url(http://a.tiles.mapbox.com/v3/marker/pin-l-000000.png)')
-            .text('Preschool')
-            .bind('click', filterPreschool);
-        container.append(el);
+        // var el = $(document.createElement('a'))
+        //     .addClass('markerfilter')
+        //     .attr('href', '#preschool')
+        //     .css('background-image', 'url(http://a.tiles.mapbox.com/v3/marker/pin-l-000000.png)')
+        //     .text('Preschool')
+        //     .bind('click', filterPreschool);
+        // container.append(el);
         
      function filterInfant(e) {
         container.find('a').removeClass('selected');
@@ -42,14 +39,14 @@ $('#map').mapbox('andyhull.map-qflr4pt1', function(map, tilejson) {
         });
         return false;
     }
-    function filterPreschool(e) {
-        container.find('a').removeClass('selected');
-        var id = $(this).addClass('selected').attr('href').replace('#', '');
-        childcarecenters.filter(function(feature) {
-            return feature.properties['preschool'] == 1 || id == 'all';
-        });
-        return false;
-    }
+    // function filterPreschool(e) {
+    //     container.find('a').removeClass('selected');
+    //     var id = $(this).addClass('selected').attr('href').replace('#', '');
+    //     childcarecenters.filter(function(feature) {
+    //         return feature.properties['preschool'] == 1 || id == 'all';
+    //     });
+    //     return false;
+    // }
 
     map.setZoomRange(0, 18);
 
@@ -62,22 +59,5 @@ $('#map').mapbox('andyhull.map-qflr4pt1', function(map, tilejson) {
         return false;
     }
     map.centerzoom({lat:37.74110000000002, lon:-122.40589999999996}, 12);
-
-    // Add share control
-    // mapbox.share().map(map).add();
-
-    // $.each(childcarecenters.markers(), function(index, m) {
-    //     console.log("hey"+m);
-    //     var s = m.data.properties['infant'];
-
-    //     if (container.find('[href="#' + s + '"]').length) return;
-
-    //     var el = $(document.createElement('a'))
-    //         .addClass('markerfilter')
-    //         .attr('href', '#' + s)
-    //         .css('background-image', 'url(http://a.tiles.mapbox.com/v3/marker/pin-l-'+s+'+000000.png)')
-    //         .bind('click', filter);
-    //     container.append(el);
-    // });
 
 });
